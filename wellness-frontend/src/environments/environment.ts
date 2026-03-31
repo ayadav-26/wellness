43ba6e4187807f8e59de735d3c77936603ba0df8
Akash <ayadav@smartsight.in>
2026-03-31 11:39:03 +0530

    fix api url for prod

--- wellness-frontend/src/environments/environment.ts
+++ wellness-frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api/v1'
+  apiBaseUrl: 'https://wellness-d877.onrender.com/api/v1'
+  // apiBaseUrl: 'http://localhost:3000/api/v1'
 };
